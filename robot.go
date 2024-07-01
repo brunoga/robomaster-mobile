43136c3732abfca6230d7acb96762852105221be
Bruno Albuquerque <bga@gmail.com>
2024-07-01 08:28:30 -0400

    Expose speed levels.

--- robot.go
+++ robot.go
@@ -11,3 +11,19 @@ type Robot struct {
 func (r *Robot) BatteryPowerPercent() int8 {
 	return int8(r.r.BatteryPowerPercent())
 }
+
+// ChassisSpeedLevel returns the current chassis speed level.
+func (r *Robot) ChassisSpeedLevel() (int8, error) {
+	chassisSpeedLevel, err := r.r.ChassisSpeedLevel()
+	if err != nil {
+		return -1, err
+	}
+
+	return int8(chassisSpeedLevel), nil
+}
+
+// SetChassisSpeedLevel sets the chassis speed level. Valid values are 0 (Fast),
+// 1 (Medium) and 2 (Slow).
+func (r *Robot) SetChassisSpeedLevel(speedLevel int8) error {
+	return r.r.SetChassisSpeedLevel(robot.ChassisSpeedLevelType(speedLevel))
+}
